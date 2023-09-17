d2df3843983dc1765142f8343bc713fb6305c79f
noefernandes <noe.fernandes.016@ufrn.edu.br>
2023-09-17 18:09:56 -0300

    Navbar pronta

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+    font-family: 'Patua One', cursive;
   }
 `;
 
@@ -30,13 +31,12 @@ const Container = styled.section`
   height: 100vh;
   display: flex;
   flex-direction: column;
+  background-color: #0C1242;
 `;
 
 const Body = styled.div`
   width: 100%;
   height: auto;
-
-
 `;
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,56 +1,184 @@
+import React from 'react';
+import { useState } from 'react';
 import { styled } from 'styled-components';
-import logo from '../assets/black-transparent.png';
+import logo from '../assets/logo-no-background.svg';
+import menuHambuger from '../assets/hamburger-icon.svg';
 
 const NavBar = () => {
+    const [isVisible, setIsVisible] = useState(false)
+
+    const handleToggle = () => {
+        setIsVisible(!isVisible);
+        console.log(isVisible);
+    }
+
     return (
-        <Nav>
-            <Logo>
-                <img src={logo} alt="" />
-            </Logo>
-            <div>
-                <Link href="#home">Home</Link>
-                <Link href="#news">News</Link>
-                <Link href="#contact">Contact</Link>
-                <Link href="#about">About</Link>
-            </div>
-        </Nav>
+        <>
+            <Nav>
+                <Logo>
+                    <img src={logo} alt="" />
+                </Logo>
+                <NavList>
+                    <ul>
+                        <NavItem><NavLink>Projetos</NavLink></NavItem>
+                        <NavItem><NavLink>Habilidades</NavLink></NavItem>
+                        <NavItem><NavLink>Experiências</NavLink></NavItem>
+                        <NavItem><NavLink>Formação</NavLink></NavItem>
+                        <NavItem><NavLink>Contato</NavLink></NavItem>
+                    </ul>
+                </NavList>
+
+                <MobileMenuIcon>
+                    <button onClick={handleToggle}>
+                        <img src={menuHambuger} />
+                    </button>
+                </MobileMenuIcon>
+            </Nav>
+            <MobileMenu isVisible={isVisible}>
+                <ul>
+                    <NavItemMobile><NavLink>Teste 1</NavLink></NavItemMobile>
+                    <NavItemMobile><NavLink>Teste 2</NavLink></NavItemMobile>
+                    <NavItemMobile><NavLink>Teste 3</NavLink></NavItemMobile>
+                </ul>
+            </MobileMenu>
+        </>
     );
 }
 
 const Nav = styled.div`
-    overflow: hidden;
+    width: 100%;
+    position: relative;
+    background-color: #0666AC;
     display: flex;
-    justify-content: space-evenly;
     align-items: center;
-    height: 80px;
+    padding: 0rem 6rem;
+    font-size: 10px;
+
+    @media (max-width: 1440px){
+        padding: 0rem 2rem;
+    }
 `;
 
-const Link = styled.a`
-    float: left;
-    color: black;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-size: 17px;
+const NavList = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0rem 8rem;
+    margin-left: 60px;
 
-    &:hover {
-        background-color: #FFF;
-        color: black;
+    ul {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        list-style: none;
     }
 
-    &:active {
-        background-color: #04AA6D;
-        color: white;
+    @media (max-width: 1440px){
+        margin-left: 15px;
+    }
+
+    @media (max-width: 730px){
+        display: none;
+    }
+`;
+
+const NavItem = styled.li`
+    width: 250px;
+    text-align: center;
+
+    @media (max-width: 1440px) {
+        width: 150px;
+    }
+
+    @media (max-width: 730px) {
+        display: none;
+    }
+`;
+
+const NavItemMobile = styled.li`
+    @media not (max-width: 730px) {
+        display: none;
+    }
+`;
+
+const NavLink = styled.a`
+    display: block;
+    font-size: 1.5rem;
+    color: #FFFFFF;
+    font-weight: 500;
+    padding: 1rem 4rem;
+    width: inherit;
+
+    @media (max-width: 1440px) {
+        padding: 1rem 0.25rem;
+    }
+
+    @media (max-width: 730px) {
+        display: block;
+
+        cursor: pointer;
+        background-color: #0666AC;
+        color: #FFFFFF;      
+    }
+
+    &:hover {
+        background-color: #FFFFFF;
+        color: #0666AC;
+    } 
+`;
+
+const MobileMenuIcon = styled.div`
+    display: none;
+    position: absolute;
+    right: 0;
+    margin-right: 4rem;
+
+    button {
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+
+        img {
+            height: 30px;
+            width: 30px;
+        }
+    }
+
+    @media (max-width: 730px) {
+        display: block;
+        align-self: center;
+    }
+`;
+
+const MobileMenu = styled.div`
+    display: ${(props) => (props.isVisible ? 'block' : 'none')};
+
+    color: #0666AC;
+
+    ul {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        padding-bottom: 1rem;
+    }
+
+    ${NavItem} {
+        display: block;
+        padding-top: 1.2rem;
+    }
+
+    @media (min-width: 730px){
+        display: none;
     }
 `;
 
 const Logo = styled.div`
-    width: 80px;
-    height: auto;
+    display: flex;
+    align-items: center;
 
     img {
-        width: 100%;
-        height: auto;
+        width: 80px;
     }
 `;
 
